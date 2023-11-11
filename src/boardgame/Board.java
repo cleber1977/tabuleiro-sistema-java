f7b3e487f6c9a23a97beff42ce9779243fbfa9f4
Cleber Cardoso <clebecar@hotmail.com>
2023-11-11 15:32:45 -0300

    Criando Execacao

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,8 +4,11 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Pecas[][] pecas;
-	
+
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1 ) {
+			throw new BoardException("Erro ao cria tabuleiro: É necessario ao menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pecas = new Pecas[rows][columns];
@@ -15,28 +18,46 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
-	public Pecas pecas (int rows, int columns) {
+	public Pecas pecas(int rows, int columns) {
+		if(!positionExists(rows, columns)) {
+			throw new BoardException("Posicao não existe no Tabuleiro");
+		}
 		return pecas[rows][columns];
 	}
+
 	public Pecas pecas(Posicao position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posicao não existe no Tabuleiro");
+		}
 		return pecas[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void PlacePecas(Pecas peca, Posicao posicao) {
+		if(therIsaPiece(posicao)) {
+			throw new BoardException("Ja existe uma peca na posicao" + posicao);
+		}
 		pecas[posicao.getRow()][posicao.getColumn()] = peca;
 		peca.position = posicao;
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Posicao posicao) {
+		return positionExists(posicao.getRow(), posicao.getColumn());
+	}
+
+	public boolean therIsaPiece(Posicao posicao) {
+		if(!positionExists(posicao)) {
+			throw new BoardException("Posicao não existe no Tabuleiro");
+		}
+		return pecas(posicao) != null;
 	}
 
 }
