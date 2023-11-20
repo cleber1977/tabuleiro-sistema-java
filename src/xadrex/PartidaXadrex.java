31bfd654411a41043588ff259b3209db54b18826
Cleber Cardoso <clebecar@hotmail.com>
2023-11-20 18:57:28 -0300

    Implementacao Bispo

--- src/xadrex/PartidaXadrex.java
+++ src/xadrex/PartidaXadrex.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardgame.Board;
 import boardgame.Pecas;
 import boardgame.Posicao;
+import xadrex.pecas.Bispo;
 import xadrex.pecas.King;
 import xadrex.pecas.Peao;
 import xadrex.pecas.Rook;
@@ -189,7 +190,9 @@ public class PartidaXadrex {
 
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 1, new Bispo(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('f', 1, new Bispo(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('a', 2, new Peao(board, Color.WHITE));
 	    placeNewPiece('b', 2, new Peao(board, Color.WHITE));
@@ -201,7 +204,9 @@ public class PartidaXadrex {
 	    placeNewPiece('h', 2, new Peao(board, Color.WHITE));
 		
 	    placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+	    placeNewPiece('c', 8, new Bispo(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('f', 8, new Bispo(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 7, new Peao(board, Color.BLACK));
 		placeNewPiece('b', 7, new Peao(board, Color.BLACK));
